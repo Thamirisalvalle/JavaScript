31b7b76cc26b5be3aab69788ad18d9f070e86a88
Thamiris Meli <thamirisalvalle@gmail.com>
2024-02-02 22:20:50 -0300

    Criação de pastas para as aulas 01 e 02 e os desafios para as aulas

---
+++ Aula 02/aula-02.js
@@ -0,0 +1,77 @@
+const name = 'Bruno'
+
+const array = ['bruno', 'ana', 5, null, undefined, 10.5]
+
+const numeros = [1,2,3,4,5,6,7,8,9,0]
+
+numeros.splice(4,2,999,888,777)
+console.log(numeros)
+
+numeros.splice(9,0,'aula')
+console.log(numeros)
+
+console.log(numeros.includes('aula 1'))
+
+const matriz = [
+  [1, 2],
+  [3, 4],
+]
+
+console.log(matriz [1][1])
+
+const alunos = [
+  ['aluno 1', 7],
+  ['aluno 2', 6],
+  ['aluno 3', 1],
+  ['aluno 4', 10],
+  ['aluno 5', 9],
+  // aluno 6,7
+]
+
+console.log(alunos[3] [1])
+console.log(alunos[1])
+
+
+// Desafio 01
+/* INDEX 
+0= soma
+1= subtração
+2= multiplicação
+3= divisão  */
+
+const numero1 = 7
+const numero2 = 2
+
+const resultado = [numero1 + numero2, numero1 - numero2, numero1 * numero2,numero1 / numero2]
+
+console.log(resultado)
+console.log(resultado[0])
+console.log(resultado[1])
+console.log(resultado[2])
+console.log(resultado[3])
+
+// Desafio 02
+
+
+// Desafio 03
+
+
+// Desafio 04
+
+
+// Desafio 05
+
+
+// Desafio 06
+
+
+// Desafio 07
+
+
+// Desafio 08
+
+
+// Desafio 09
+
+
+// Desafio 10
