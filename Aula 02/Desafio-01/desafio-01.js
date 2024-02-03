c29cf916acd98d8126a5e880821484483fe3bcde
Thamiris Meli <thamirisalvalle@gmail.com>
2024-02-02 22:31:07 -0300

    Criação dos arquivos de desafios da aula 02 e resolução do desafio 01

---
+++ Aula 02/Desafio-01/desafio-01.js
@@ -0,0 +1,17 @@
+// Desafio 01
+/* INDEX 
+0= soma
+1= subtração
+2= multiplicação
+3= divisão  */
+
+const numero1 = 7
+const numero2 = 2
+
+const resultado = [numero1 + numero2, numero1 - numero2, numero1 * numero2,numero1 / numero2]
+
+console.log(resultado)
+console.log(resultado[0])
+console.log(resultado[1])
+console.log(resultado[2])
+console.log(resultado[3])
