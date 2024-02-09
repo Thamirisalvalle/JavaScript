9963338c00b5515039cdbf1b05307948af27fb7f
Thamiris Meli <thamirisalvalle@gmail.com>
2024-02-09 19:10:02 -0300

    Realização do desafio 01 e 02

---
+++ Aula 04/Desafio-01/Desafio-01.js
@@ -0,0 +1,17 @@
+// Desafio 01
+
+// condição if
+const numero1 = 6
+const numero2 = 9
+
+if (numero1 % 2 == 0) {
+  console.log("É par");
+} else {
+  console.log("É impar");
+}
+
+// condição ternário
+
+const ehImpar = numero2 % 2 == 0 ? 'É par' : 'É impar'
+
+console.log(ehImpar)
---
+++ Aula 04/Desafio-02/Desafio-02.js
@@ -0,0 +1,14 @@
+// Desafio 02
+
+// Condição if
+const nota = 90
+
+if (nota >= 90){
+  console.log("Aprovado com mérito");
+} else if (nota >= 70 && nota < 90) {
+  console.log("Aprovado");
+} else {
+  console.log("Reprovado");
+}
+
+// else é usado para basicamente finalizar quando o programa já passou por todaas condições e deu false, e ele é de uso opcional.
