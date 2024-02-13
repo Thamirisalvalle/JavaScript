1f40b1808e6a5cb5ea07d2469baab4425585086b
Thamiris Meli <thamirisalvalle@gmail.com>
2024-02-13 17:37:52 -0300

    Atualização de anotações da aula 01

--- Aula 01/Aula-01.js
+++ Aula 01/Aula-01.js
@@ -1,3 +1,5 @@
+// Constantes e Variáveis
+
 let contador = 2
 contador = 10
 
@@ -8,3 +10,30 @@ let nome ="Thamiris"
 
 console.log (contador+x)
 
+// Funções nativas
+
+console.log ("Hello World") // Saída no console
+console.warn ("Hello World") // Sinaliza como atenção
+console.error ("Hello World") // Sinaliza algum erro
+
+prompt ("Digite seu nome") // abre caixa de dialogo para receber o valor
+alert ("Hello World") // Pop-up 
+
+// Exemplos 
+
+let name = prompt ("Digite o nome: ")
+console.log("Olá " + name)
+
+alert("Hello Thamiris")
+
+// Coerções
+const numero1 = 5
+const numero2 = "10"
+
+console.log(numero1+numero2)
+
+// Transformando em número
+const numero3 = 5
+const numero4 = parseInt("10")
+
+console.log(numero3+numero4)
